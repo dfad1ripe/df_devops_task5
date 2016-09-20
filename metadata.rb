9a88bcce3b708a749779baa3b1c214a1e90de69b
Dmytro <dfad1ripe@gmail.com>
2016-09-20 11:10:40 +0300

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'Task5'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures Task5'
+long_description 'Installs/Configures Task5'
+version '0.1.0'
