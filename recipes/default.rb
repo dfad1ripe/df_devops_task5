34eb5bbfc14bb129e32a677f1e91c6fca7c16ba5
Dmytro <dfad1ripe@gmail.com>
2016-09-20 17:29:45 +0300

    failed 1

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,26 @@
 #
 # Cookbook Name:: Task5
 # Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+gInstaller = "#{Chef::Config['file_cache_path']}\
+Greenshot-INSTALLER-1.2.8.12-RELEASE.exe"
+
+gIniFile = "#{Chef::Config['file_cache_path']}greenshot-defaults.ini"
+
+
+cookbook_file gInstaller do
+  source 'Greenshot-INSTALLER-1.2.8.12-RELEASE.exe'
+  #not_if 'yum list | grep mysql57-community-release'
+end
+
+cookbook_file gIniFile do
+  source 'greenshot-defaults.ini'
+  #not_if 'yum list | grep mysql57-community-release'
+end
+
+
+#execute 'mysql community rpm installation' do
+#  command "rpm -ivh #{comm_rpm}"
+#  not_if 'yum list | grep mysql57-community-release'
+#end
+
