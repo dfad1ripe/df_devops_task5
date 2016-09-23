607c2e9be4c44753ff7099eef33fa2b942859d83
Dmytro <dfad1ripe@gmail.com>
2016-09-23 06:32:44 +0300

    Integration tests Ok. Unit tests fail on 'not_if' constructions, put pass if those are commented out.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -4,17 +4,25 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-require 'spec_helper'
+require 'spec_helper.rb'
 
 describe 'Task5::default' do
-  context 'When all attributes are default, on an unspecified platform' do
+  context 'test:' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2012r2')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'installs "Greenshot"' do
+      expect(chef_run).to install_windows_package('greenshot')
+    end
+
+    it 'installs Hyper-V PS module' do
+      expect(chef_run).to install_windows_feature('Microsoft-Hyper-V-Management-PowerShell')
     end
   end
 end
--- test/recipes/default/default_test.rb
+++ test/recipes/default/default_test.rb
@@ -5,9 +5,17 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
+# describe package ('Greenshot 1.2.8.12') do
+#   it { should be_installed }
+# end
+
 describe command('"C:\Program Files\Greenshot\Greenshot.exe" /reload') do
   its(:exit_status) { should eq 0 }
 end
+
+# describe windows_feature('Microsoft-Hyper-V-Management-PowerShell') do
+#   it { should be_installed }
+# end
 
 describe command('powershell Get-Module -ListAvailable') do
   its(:stdout) { should match 'Hyper-V' }
